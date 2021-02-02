9977e49c8f4afbedad9b87a29853c151e5ed5631
remihayrimbault <remi.hayrimbault@gmail.com>
2021-02-02 10:22:55 +0100

    Bye Webpack

--- Webpack-project/postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = ({options}) => ({
-    plugins: [
-        require('autoprefixer')({grid: 'autoplace'}),
-        require('postcss-flexbugs-fixes'),
-        options.env === 'production' ? require('cssnano') : false,
-    ]});
